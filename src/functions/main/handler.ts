34472f94cdc0bb55a98e4bc345f0defac02fc591
ryochansq <nicestsnsq@gmail.com>
2022-09-02 00:06:01 +0900

    add images

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,11 +1,31 @@
-import { formatJSONResponse } from '@libs/api-gateway';
+import * as fs from 'fs';
 import { middyfy } from '@libs/lambda';
 
-const main = async (event) => {
-  return formatJSONResponse({
-    message: `ほげhoge`,
-    event,
-  });
+const getAllStudents = (): string[] => {
+  const path = process.cwd();
+  const filenames = fs.readdirSync(`${path}/src`);
+  console.log({ path, filenames });
+  return [];
+};
+
+const select4Students = (allStudents: string[]): string[] => {
+  return [];
+};
+
+const concatImages = (students: string[]): any => {};
+
+const tweetImage = (image: any): string => {
+  return '';
+};
+
+const tweetText = (students: string[], imageId: string): void => {};
+
+const main = async () => {
+  const allStudents = getAllStudents();
+  const students = select4Students(allStudents);
+  const image = concatImages(students);
+  const imageId = tweetImage(image);
+  tweetText(students, imageId);
 };
 
 export const handler = middyfy(main);
