73ef9eef8e5f9f72bdcc7298088b66ff33443b5a
ryochansq <nicestsnsq@gmail.com>
2022-09-02 01:00:46 +0900

    fix

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -22,57 +22,7 @@ const selectFourStudents = (allStudents: string[]): string[] => {
   return shuffled.slice(0, 4);
 };
 
-const concatImages = (students: string[]): any => {
-  (async () => {
-    const imagePaths = students.map((student) => `${student}.jpg`);
-    const imageAttrs = [];
-
-    // 連結する画像の情報取得
-    const promises = [];
-    const imagePromise = (path) =>
-      new Promise(async (resolve) => {
-        const image = await sharp(path);
-        let width = 0,
-          height = 0;
-        await image
-          .metadata()
-          .then((meta) => ([width, height] = [meta.width, meta.height]));
-        const buf = await image.toBuffer();
-        resolve({ width, height, buf });
-      });
-    imagePaths.forEach((path) => promises.push(imagePromise(path)));
-    await Promise.all(promises).then((values) => {
-      values.forEach((value) => imageAttrs.push(value));
-    });
-
-    // outputする画像の設定
-    const outputImgWidth = imageAttrs.reduce((acc, cur) => acc + cur.width, 0);
-    const outputImgHeight = Math.max(...imageAttrs.map((v) => v.height));
-    let totalLeft = 0;
-    const compositeParams = imageAttrs.map((image) => {
-      const left = totalLeft;
-      totalLeft += image.width;
-      return {
-        input: image.buf,
-        gravity: 'northwest',
-        left: left,
-        top: 0,
-      };
-    });
-
-    // 連結処理
-    sharp({
-      create: {
-        width: outputImgWidth,
-        height: outputImgHeight,
-        channels: 4,
-        background: { r: 255, g: 255, b: 255, alpha: 0 },
-      },
-    })
-      .composite(compositeParams)
-      .toFile('output.png');
-  })();
-};
+const concatImages = (students: string[]): any => {};
 
 const tweetImage = (image: any): string => {
   return '';
