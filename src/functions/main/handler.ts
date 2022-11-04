d6ec87a0af459769ed932c1ea372c5dc82705e59
ryochansq <nicestsnsq@gmail.com>
2022-11-04 11:39:58 +0900

    fix students type

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,38 +1,23 @@
 import * as fs from 'fs';
 import { middyfy } from '@libs/lambda';
+import _ from 'lodash';
+import { Student, students } from 'src/students/list';
 
-// https://www.nxworld.net/js-array-shuffle.html
-const shuffle = ([...array]) => {
-  for (let i = array.length - 1; i >= 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+const imageFileDir = `${process.cwd()}/src/students/images`;
+
+const concatImages = (students: Student[]): any => {
+  const imagePaths = students.map((name) => `${imageFileDir}/${name}.jpg`);
 };
-
-const getAllStudents = (): string[] => {
-  const path = process.cwd();
-  const fileNames = fs.readdirSync(`${path}/src/images`);
-  return fileNames.map((name) => name.slice(0, -4)); // 末尾「.jpg」の４文字分を削っている
-};
-
-const selectFourStudents = (allStudents: string[]): string[] => {
-  const shuffled = shuffle(allStudents);
-  return shuffled.slice(0, 4);
-};
-
-const concatImages = (students: string[]): any => {};
 
 const tweetImage = (image: any): string => {
   return '';
 };
 
-const tweetText = (students: string[], mediaId: string): void => {};
+const tweetText = (students: Student[], mediaId: string): void => {};
 
 const main = async () => {
-  const allStudents = getAllStudents();
-  const fourStudents = selectFourStudents(allStudents);
-  console.info(fourStudents);
+  const fourStudents = _.shuffle(students).slice(0, 4);
+  console.info(...fourStudents);
   const image = concatImages(fourStudents);
   const mediaId = tweetImage(image);
   tweetText(fourStudents, mediaId);
