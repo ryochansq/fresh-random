d7667b66d7fd1183f919bfe6783ecbbebd09c10f
ryochansq <nicestsnsq@gmail.com>
2022-11-05 23:14:17 +0900

    test

--- src/functions/main/index.ts
+++ src/functions/main/index.ts
@@ -12,7 +12,7 @@ export default {
     {
       schedule: {
         name: 'fresh-random',
-        rate: ['cron(14 14 ? * SAT *)'],
+        rate: ['cron(18 14 ? * SAT *)'],
       },
     },
   ],
